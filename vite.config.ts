dfbd66ec28760fb2ff51efa048b582c8290c6663
Jake Klassen <jklassendev@gmail.com>
2025-12-20 14:08:02 -0500

    updated build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     target: 'esnext',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        fullscreen: resolve(__dirname, 'fullscreen.html'),
+        'fullscreen-css': resolve(__dirname, 'fullscreen-css.html'),
+      },
+    },
   },
 });
